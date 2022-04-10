8f585f46e0852e6b11d1c23c28b654cbf02bc781
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-04-10 03:36:06 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Gabriel D. Padua on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppCounter
+//
+//  Created by Gabriel D. Padua on 10/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
